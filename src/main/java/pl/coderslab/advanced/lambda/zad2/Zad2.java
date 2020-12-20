810abed492a9a9d9584755c8f23fc54d5834ed4b
Rafal <rafal.opial@coderslab.pl>
2020-12-20 11:51:58 +0100

    m6d2t3z2 test if string

---
+++ src/main/java/pl/coderslab/advanced/lambda/zad2/Zad2.java
@@ -0,0 +1,26 @@
+package pl.coderslab.advanced.lambda.zad2;
+//
+//## Zadanie 2 - rozwiązywane z wykładowcą
+//
+//        1. Korzystając z interfejsu funkcyjnego Predicate napisz wyrażenie lambda,
+//        które będzie sprawdzało czy podany parametr jest typu znakowego (String) i jeśli tak,
+//        to wypisze go na ekranie
+
+import java.util.function.Predicate;
+
+public class Zad2 {
+    public static void main(String[] args) {
+        Predicate isString = o -> o instanceof String;
+
+        int intVal = 42;
+        if(isString.test(intVal)){
+            System.out.println(intVal);
+        }
+
+        String strVal = "to jest napis";
+        if(isString.test(strVal)){
+            System.out.println(strVal);
+        }
+
+    }
+}
